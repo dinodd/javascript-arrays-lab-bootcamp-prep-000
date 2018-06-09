467c85e7fb06648544f2d1790dbf906ec2de7019
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-09 08:05:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const app = "I don't do much."
 
-function arrays() {
-  kittens = ['Milo', 'Otis', 'Garfield'];
-  return kittens;
-}
+kittens = ['Milo', 'Otis', 'Garfield'];
 
-function kittens() {
-  var kittens  = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
   return kittens;
 }
